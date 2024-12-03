a836c1722a69dab02655c79f97138675a29c0f24
Pedro Santana <pedromanu27@gmail.com>
2024-12-02 21:46:51 -0300

    fix: :ambulance: update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import svgr from 'vite-plugin-svgr';
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), svgr()],
-	base: '/santana-morais-advocacia-lp/',
+	base: '/dist/',
 });
